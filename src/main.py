b3e2608feb47686a1f379b43b6ffca4e5fba4e03
Faridun <elmurodovfaridun24@gmail.com>
2026-09-19 05:50:33 -0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from openpyxl import load_workbook
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+DATA_FILE = PROJECT_ROOT / "data" / "student.xlsx"
+
+def load_students(file_path: Path):
+    workbook = load_workbook(file_path, data_only=True)
+
+    worksheet = workbook.active
+
+    students = []
+
+    for row_number, row in enumerate(worksheet.iter_rows(min_row=2, values_only=True), start=2):
+        full_name, award, date = row
+
+        students.append(
+            {
+                "row_number": row_number,
+                "full_name": full_name,
+                "award": award,
+                "date": date,
+            }
+        )
+
+        return students
+
+
+students = load_students(DATA_FILE)
+
+for student in students:
+    print(student)
